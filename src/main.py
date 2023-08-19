f59e07cc421b6d2ce8ba97a0ef0e697b8c0b9031
bluebirz <9207891+bluebirz@users.noreply.github.com>
2023-08-19 16:33:49 +0200

    prep

---
+++ src/main.py
@@ -0,0 +1,14 @@
+import apache_beam as beam
+
+
+def run():
+    options = beam.options.pipeline_options.PipelineOptions()
+    # with beam.Pipeline() as pipe:
+    #     out = (
+    #         pipe | "read" >> beam.
+    #     )
+    pass
+
+
+if __name__ == "__main__":
+    run()
