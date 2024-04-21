a41d9b6f8e191aece8788d7507887e8e266a5159
bluebirz <9207891+bluebirz@users.noreply.github.com>
2024-04-21 12:33:11 +0200

    move 06d to 07

--- 07-custom-io/src/functions/firestore_reader.py
+++ 07-custom-io/src/functions/firestore_reader.py
@@ -1,4 +1,3 @@
-import apache_beam as beam
 from apache_beam.io import iobase, OffsetRangeTracker
 from apache_beam.metrics import Metrics
 import google.auth
@@ -36,47 +35,6 @@ class FireStoreReaderFromCollection(iobase.BoundedSource):
         docs = self.target_collection.stream()
         for doc in docs:
             yield {"key": doc.id, "value": doc.to_dict()}
-
-    def estimate_size(self):
-        return self._count
-
-    def get_range_tracker(self, start_position, stop_position):
-        if start_position is None:
-            start_position = 0
-        if stop_position is None:
-            stop_position = self._count
-
-        return OffsetRangeTracker(start_position, stop_position)
-
-    def read(self, range_tracker):
-        for i in range(range_tracker.start_position(), range_tracker.stop_position()):
-            if not range_tracker.try_claim(i):
-                return
-            self.records_read.inc()
-            yield i
-
-    def split(self, desired_bundle_size, start_position=None, stop_position=None):
-        if start_position is None:
-            start_position = 0
-        if stop_position is None:
-            stop_position = self._count
-
-        bundle_start = start_position
-        while bundle_start < stop_position:
-            bundle_stop = min(stop_position, bundle_start + desired_bundle_size)
-            yield iobase.SourceBundle(
-                weight=(bundle_stop - bundle_start),
-                source=self,
-                start_position=bundle_start,
-                stop_position=bundle_stop,
-            )
-            bundle_start = bundle_stop
-
-
-class CountingSource(iobase.BoundedSource):
-    def __init__(self, count):
-        self.records_read = Metrics.counter(self.__class__, "recordsRead")
-        self._count = count
 
     def estimate_size(self):
         return self._count
